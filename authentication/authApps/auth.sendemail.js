63877d17d1810f1f9453b2d147f7851558fe14e2
郭朋振 <guopengzhen@kakutomooburis-MacBook-Pro.local>
2023-01-21 09:57:24 +0800

    update

--- authentication/authApps/auth.sendemail.js
+++ authentication/authApps/auth.sendemail.js
@@ -14,15 +14,19 @@ var mailOptions = {
   subject: 'Sending Email using Node.js',
   html: '<p>Click <a href="http://localhost:3000/signup/emailverify/' + hashid + '">here</a> to verify your email</p>'
 };
+var nodemailer = require('nodemailer');
 
-module.exports.sendMail = function (email,html) {
-    mailOptions.to = email;
-    mailOptions.html = html;
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
-}
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'pengzhen@gmail.com',
+    pass: 'yourpassword'
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
